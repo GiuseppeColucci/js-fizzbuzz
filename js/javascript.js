c6934eb4617068a4663417ec5bc71e8b7ef12233
giuseppecolucci <giuseppecolucciwork@gmail.com>
2020-10-20 15:51:35 +0200

    finito

--- js/javascript.js
+++ js/javascript.js
@@ -1,19 +1,21 @@
 var num=Math.round(Math.random(0)*101);
-var per3=(num*100%3 !=0);
-var per5=(num*100%5 !=1);
+var per3=(num%3 ==0);
+var per5=(num%5 ==0);
 
 
 
 
-
-if(per3==true){
-    alert(num+'fizz')
+if(per3 == true && per5 == true){
+    alert(num + 'FizzBuzz')
 }
 else if (per5==true){
-    alert(num+'buzz')
+    alert(num+'BUZZ')
 }
-else if(per3==true&&per5==true){
-    alert(num+'BUZZFIZZ')
+else if( per3==true){
+    alert(num+'FIZZ')
+}
+else{
+    alert(num)
 }
 
 
