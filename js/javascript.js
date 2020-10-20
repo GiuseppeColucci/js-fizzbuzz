7469f9ec3b23047f2723abd2326b51ced72811f2
giuseppecolucci <giuseppecolucciwork@gmail.com>
2020-10-20 14:32:33 +0200

    devo solo ordinare il tutto!!

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,23 @@
-console.log('works giuseppe colucci')
+var num=Math.round(Math.random(0)*101);
+console.log(num)
+if(num==100%3){
+    alert(num+'fizz')
+}
+else if (num==100%5){
+    alert(num+'buzz')
+}
+else if(num==100%5)&&(num==100%3){
+    alert(num+'buzzfizz')
+    
+}
+else{
+    alert(num)
+}
+
+
+
+// (100%3!=0)=fizz
+// (100%5!=0)=buzz
+// if(x(100%3!=0)&&(100%5!=0){
+//     console.log('FIZZBUZZ')
+// }
